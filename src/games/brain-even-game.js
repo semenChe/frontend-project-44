ffff699517c6ff58bbb00aaf710172675ea41f42
Semen Cernyshev <semen_ch@vk.com>
2022-10-01 12:44:48 +0300

    modified brain-even-game

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,7 +1,7 @@
 import gameEngine from '../index.js';
 import randomNumber from '../helper.js';
 
-const description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+const description = "Answer 'yes' if number even otherwise answer 'no'.";
 const isEven = (number) => number % 2 === 0;
 const getQuestionAndAnswer = () => {
   const anotherGameQuestion = randomNumber(1, 100);
