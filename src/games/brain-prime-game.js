9f9654fbbd7068a0142e881431c62f8872e72dd5
Semen Cernyshev <semen_ch@vk.com>
2023-03-30 13:39:51 +0300

    Additional edits 1

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -1,7 +1,7 @@
 import gameEngine from '../index.js';
 import randomNumber from '../helper.js';
 
-const description = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (n) => {
   for (let i = 2; i <= n / 2; i += 1) {
     if (n % i === 0) {
